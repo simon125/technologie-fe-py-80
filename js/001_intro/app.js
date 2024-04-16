ceacfa1c4a8698e795fd3b9eaff95f685a03899d
Szymon Oleszek <szymqwerty@gmail.com>
2024-04-16 17:32:44 +0200

    changes

--- js/001_intro/app.js
+++ js/001_intro/app.js
@@ -9,21 +9,11 @@
  *
  */
 
-// var numberOfUsers = 5;
-
-// const numberOfPosts = 5;
-
-// let numberOfTodos = 5;
-
-// numberOfUsers = 10;
-
 /**
  * console.log
  *
  * narzędzia deweloperskie
  */
-
-// console.log(numberOfUsers);
 
 /**
  * typy danych
@@ -43,41 +33,11 @@
  *
  */
 
-// let numberOfUsers = 20; // typ number
-
-// numberOfUsers = "test"; // typ number można nadpisać innym typem
-
-// const NumberOfUsers = "123123"; // JS jest case sensitive
-
-// console.log(numberOfUsers);
-
-// console.log(NumberOfUsers);
-
 /**
  * funkcje warunkowe
  *
  *
  */
-
-// if (123 == "123") {
-//   console.log("To są te same dane");
-// }
-
-// if (123 === "123") {
-//   console.log("To są te same dane");
-// } else {
-//   console.log("to nie są te same dane");
-// }
-
-// let numberOfUsers = 5000;
-
-// if (numberOfUsers > 30 && numberOfUsers < 100) {
-//   console.log("Mamy więcej niż 30 osób trzeba wynaąć wiekszą sale");
-// } else if (numberOfUsers <= 30) {
-//   console.log("Mniej niż 30");
-// } else {
-//   console.log("Pogubiłem się");
-// }
 
 /**
  * Zadanie:
@@ -88,58 +48,10 @@
  * dodatkowo sprawdzaj czy wiek ma znaczenie dodając sprawdzenie w warunku zmiennej doesAgeMatter
  */
 
-// if (age >= 18) {
-//   console.log("Użytkownik jest dorosły");
-// } else if (age < 18) {
-//   console.log("Użytkownik jest za młody");
-//   console.log(123);
-// }
-
-// // if (doesAgeMatter === true) {
-// if (doesAgeMatter) {
-//   // jeżeli mamy zmienną booleanową nie musimy porównywać jej do true lub false
-//   if (age >= 18) {
-//     console.log("user is adult");
-//   } else {
-//     console.log("user is too young");
-//   }
-// } else {
-//   console.log("You have access");
-// }
-
-// if (age >= 18) {
-//   console.log("uzytkownik dorosly");
-// } else {
-//   console.log("dziecko");
-// }
-
 /**
  * struktury danych
  * obiekty (słowniki) i tablice
  */
-
-// const namesOfUsers = ["John", "Jane", "Jack", 123, true];
-
-// const user1 = {
-//   name: "Jack",
-//   age: 10,
-//   gender: "male",
-//   isAdult: false,
-// };
-
-// const user2 = {
-//   name: "Jack",
-//   age: 10,
-//   gender: "male",
-//   isAdult: false,
-// };
-
-// const user3 = {
-//   name: "Jack",
-//   age: 10,
-//   gender: "male",
-//   isAdult: false,
-// };
 
 /**
  * Porównywanie obiektów za pomocą JSONa
@@ -151,19 +63,6 @@
 /**
  * Obiekty są porównywane za pomocą referencji nie jest porównywana struktura
  */
-// if (user1  === user2) {
-//   console.log("te obiekty są takie same");
-// }
-
-// if (JSON.stringify(user1) === JSON.stringify(user2)) {
-//   console.log("te obiekty są takie same");
-// }
-
-// const users = [user1, user2, user3];
-
-// console.log(user1);
-
-// console.table(users);
 
 /**
  * Zdanie:
@@ -182,39 +81,6 @@
 //  * rezultat możesz wykonsolować za pomocą console.table(users)
 //  */
 
-// const todos = ["Wstać rano", "Zjeść śniadanie"];
-
-// const user1 = {
-//   name: "John",
-//   lastName: "Wick",
-//   age: 45,
-//   todos: todos,
-// };
-
-// console.log(user1);
-
-// const user2 = {
-//   name: "Bill",
-//   lastName: "Cosby",
-//   age: 23,
-//   todos: ["zrobić zakupy", "ugotować obiad", "wyprowadzić psa"],
-// };
-// console.log(user2);
-// const user3 = {
-//   name: "John",
-//   lastName: "Rambo",
-//   age: 70,
-//   todos: ["zrobić bicki", "przebiec maraton", "przygotować napój proteinowy"],
-// };
-// const user4 = {
-//   name: "Janet",
-//   lastName: "Jackson",
-//   age: 45,
-//   todos: ["zaśpiewać solo", "zatańczyć z nieznajomym", "wystąpić w klubie"],
-// };
-// uzytkownicy = [user2, user3, user4];
-// console.table(uzytkownicy);
-
 /**
  * pętle
  *
@@ -224,30 +90,6 @@
  *
  * map, filter, forEach
  */
-
-// for (let i = 0; i <= 100; i++) {
-//   console.log(`Index: ${i}`);
-// }
-
-// const todos = ["Wstać rano", "Zjeść śniadanie"];
-// const blogPosts = [
-//   { title: "test123", content: "riuhrguifheriuh uihgeiuhg", isRead: true },
-//   { title: "tets222", content: "hdfhfghfd gh dfgh uihgeiuhg", isRead: false },
-// ];
-
-// for (let i = 0; i < todos.length; i++) {
-//   console.log(todos[i]);
-// }
-
-// console.table(blogPosts);
-
-// for (let i = 0; i < blogPosts.length; i++) {
-//   if (blogPosts[i].isRead === false) {
-//     blogPosts[i].isRead = true;
-//   }
-// }
-
-// console.table(blogPosts);
 
 /**
  * Zadanie
@@ -280,24 +122,6 @@
 //   age: 15,
 // };
 
-// const users = [user1, user2, user3, user4];
-
-// console.table(users);
-
-// for (let i = 0; i < users.length; i++) {
-//   if (users[i].age >= 18) {
-//     users[i].isAdult = true;
-//   } else {
-//     users[i].isAdult = false;
-//   }
-// }
-
-// for (let i = 0; i < users.length; i++) {
-//   users[i].isAdult = users[i].age >= 18;
-// }
-
-// console.table(users);
-
 /**
  * funkcje
  *
@@ -305,23 +129,6 @@
  *
  * arrow functions
  */
-
-// function addTwoNumbers(number1, number2) {
-//   const suma = number1 + number2;
-
-//   return suma;
-// }
-
-// const addTwoNumbers = (number1, number2) => number1 + number2;
-
-const addTwoNumbers = (number1, number2) => {
-  return number1 + number2;
-};
-
-console.log(addTwoNumbers(20, 20));
-console.log(addTwoNumbers(10, 20));
-console.log(addTwoNumbers(50, 200));
-console.log(addTwoNumbers(60, 20));
 
 /**
  * Zadanie:
@@ -350,24 +157,6 @@ console.log(addTwoNumbers(60, 20));
 //   lastName: "Rambo",
 //   age: 70,
 // };
-
-// const isUserAdult = (user) => {
-//   if (user && typeof user.age === "number") {
-//     if (user.age >= 18) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   } else {
-//     console.log("Podałeś nie poprawne dane");
-//   }
-// };
-
-// isUserAdult(undefined);
-
-// // const isUserAdult = (user) => user.age >= 18;
-
-// console.log(isUserAdult(user3));
 
 function getNumbersOfAdultsPerGender() {
   /**
